5ef70ffc46253ba0d8b0948e22e675612120ce4e
zhouxu <794135759@qq.com>
2017-09-06 12:20:13 +0800

    vux

--- resources/assets/js/components/Index.vue
+++ resources/assets/js/components/Index.vue
@@ -1,16 +1,68 @@
 <template>
     <div class="container">
-        Index
+        <group title="禁用内置验证及显示成功或者错误样式">
+      <x-input title="禁用验证" placeholder="I'm placeholder" novalidate :icon-type="iconType" :show-clear="false" @on-blur="onBlur" placeholder-align="right"></x-input>
+    </group>
     </div>
 </template>
 
 <script>
-    export default {
-        data(){
-            return {}
-        },
-        mounted() {
-            console.log('Component mounted.')
+import { XInput, Group, XButton, Cell } from 'vux'
+
+export default {
+  components: {
+    XInput,
+    XButton,
+    Group,
+    Cell
+  },
+  data () {
+    return {
+      password: '123465',
+      password2: '',
+      enterText: '',
+      valid1: false,
+      valid2: false,
+      iconType: '',
+      be2333: function (value) {
+        return {
+          valid: value === '2333',
+          msg: 'Must be 2333'
         }
+      },
+      style: '',
+      disabledValue: 'hello',
+      debounceValue: '',
+      maxValue: ''
     }
+  },
+  methods: {
+    getValid1 () {
+      this.valid1 = this.$refs.input01.valid
+    },
+    getValid2 () {
+      this.valid2 = this.$refs.input02.valid
+    },
+    change (val) {
+      console.log(val)
+    },
+    onBlur (val) {
+      console.log('on blur', val)
+    },
+    onFocus (val, $event) {
+      console.log('on focus', val, $event)
+    },
+    onEnter (val) {
+      console.log('click enter!', val)
+    }
+  }
+}
 </script>
+<style scoped>
+.red {
+  color: red;
+}
+.green {
+  color: green;
+}
+</style>
