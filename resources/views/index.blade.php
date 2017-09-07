20f688feb86bf7ff5f12c809ba61d402185d75f4
zhouxu <794135759@qq.com>
2017-09-07 12:39:54 +0800

    title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{csrf_token()}}">
-    <title>test</title>
+    <title>新生报到</title>
     <style>
     html{
         background-color: rgba(0, 188, 212, 0.15);
